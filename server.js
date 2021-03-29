38b8b1283c156d6d7033b6beae89c8bc1c074286
naeemmusamh <naeem@DESKTOP-RJ6QHBJ.localdomain>
2021-03-29 15:29:57 +0300

    some solve

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 //As early as possible in your application, require and configure dotenv.
@@ -20,10 +19,10 @@ const PORT = process.env.PORT || 9901;
 //A constructor function will ensure that each object is created according to the
 //same format when your server receives the external data.
 function Locations(data) {
-    this.searchQuery = select;
-    this.display_name = data.display_name;
-    this.lat = data.lat;
-    this.lon = data.lon;
+    this.searchquery = select;
+    this.formatted_query = data.display_name;
+    this.latitude = data.lat;
+    this.longitude = data.lon;
 }
 
 //A constructor function will ensure that each object is created according to the
@@ -35,14 +34,14 @@ function Weathers(data) {
 
 //Create a route with a method and a path.
 //invoke a function to convert the search query to a latitude and longitude.
-app.get('/data/location', function(request, response) {
+app.get('./data/location', function(request, response) {
     const searchQuery = request.query;
     const select = searchQuery.city;
     if (!select) {
         response.status(500).send('sorry, no city was found');
         // throw new Error('i dont find any city');
     }
-    const locationRow = require('data/location.json');
+    const locationRow = require('./data/location.json');
     const locationData = new Locations(locationRow[0], select);
     return response.send(locationData);
 });
@@ -50,8 +49,8 @@ app.get('/data/location', function(request, response) {
 //Create a route with a method and a path.
 //weather object of the result, return an array of objects for each day of the
 //response which contains the necessary information for correct client rendering.
-app.get('/data/weather', function(request, response) {
-    const weatherRow = require('data/weather.json');
+app.get('./data/weather', function(request, response) {
+    const weatherRow = require('./data/weather.json');
     const result = [];
     weatherRow.nearby_weather.forEach(element => {
         const weatherData = new Weathers(element.weather.description, element.valid_date);
