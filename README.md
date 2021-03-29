6afc08473e2b3dab04ea5ed28b26c0d0c746ba8d
naeemmusamh <naeem@DESKTOP-RJ6QHBJ.localdomain>
2021-03-30 00:27:40 +0300

    some work in lab07

--- README.md
+++ README.md
@@ -3,21 +3,28 @@
 # Project Name
 
 **Author**: Naeem Musamh
-**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+**Version**: 1.5.0
 
 ## Overview
-<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+it helps the user to get the weather to location specific
 
 ## Getting Started
 <!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
 
 ## Architecture
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+[City Explorer Application](https://codefellows.github.io/code-301-guide/curriculum/city-explorer-app/front-end/)
+[Node JS Docs](https://nodejs.org/en/)
+[Express JS Docs](http://expressjs.com/en/4x/api.html)
+[dotenv Docs](https://www.npmjs.com/package/dotenv)
+[location.json](https://codefellows.github.io/code-301-guide/curriculum/class-06/lab/starter-code/data/location.json)
+[weather.json](https://codefellows.github.io/code-301-guide/curriculum/class-06/lab/starter-code/data/weather.json)
+[Superagent Docs](https://visionmedia.github.io/superagent/)
+[Location IQ Geocoding API](https://locationiq.com/docs#forward-geocoding)
+[Weather Bit API Docs](https://www.weatherbit.io/)
 
 ## Change Log
-Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
-
-01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
+29-03-2021 11:30pm - The application now has a functional express server, with a GET route for the location resource.
+30-03-2021 12:00pm - Add API KEY
 
 ## Credits and Collaborations
 # The link for the website help me with this work
--- server.js
+++ server.js
@@ -9,17 +9,22 @@ const express = require('express');
 //Creates an cors application.
 const cors = require('cors');
 
+//create an superagent application
+const superagent = require('superagent');
+
+//Creates an env application.
+const PORT = process.env.PORT;
+const LOCATION_API_KEY = process.env.LOCATION_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
 //the app setup
 const app = express();
 app.use(cors());
 
-//Creates an env application.
-const PORT = process.env.PORT || 9901;
-
 //A constructor function will ensure that each object is created according to the
 //same format when your server receives the external data.
-function Locations(data) {
-    this.searchquery = select;
+function Locations(data, search_query) {
+    this.search_query = search_query.select;
     this.formatted_query = data.display_name;
     this.latitude = data.lat;
     this.longitude = data.lon;
@@ -34,29 +39,43 @@ function Weathers(data) {
 
 //Create a route with a method and a path.
 //invoke a function to convert the search query to a latitude and longitude.
-app.get('./data/location', function(request, response) {
-    const searchQuery = request.query;
-    const select = searchQuery.city;
-    if (!select) {
-        response.status(500).send('sorry, no city was found');
+app.get('/location', function(request, response) {
+    const searchQuery = request.query.city;
+    // const select = searchQuery.city;
+    const url = 'https://eu1.locationiq.com/v1/search.php';
+    const cityQuery = {
+        key: LOCATION_API_KEY,
+        city: searchQuery,
+        format: 'json',
+    };
+    console.log(cityQuery);
+    if (!searchQuery) {
+        response.status(404).send('sorry, no search query was found');
         // throw new Error('i dont find any city');
     }
-    const locationRow = require('./data/location.json');
-    const locationData = new Locations(locationRow[0], select);
-    return response.send(locationData);
+    superagent.get(url).query(cityQuery).then(responseData => {
+        const locationData = new Locations(searchQuery, responseData.body[0]);
+        response.status(200).send(locationData);
+    }).catch((error) => {
+        console.log('ereor', error);
+        response.staus(500).send('sorry, something wrong');
+    });
 });
 
 //Create a route with a method and a path.
 //weather object of the result, return an array of objects for each day of the
 //response which contains the necessary information for correct client rendering.
-app.get('./data/weather', function(request, response) {
-    const weatherRow = require('./data/weather.json');
-    const result = [];
-    weatherRow.nearby_weather.forEach(element => {
+app.get('/weather', function(request, response) {
+    let city;
+    const url = `https://api.weatherbit.io/v2.0/history/daily?key${WEATHER_API_KEY}&city=${city}`;
+    // const weatherRow = require('./data/weather.json');
+    superagent.get(url).then(element => {
         const weatherData = new Weathers(element.weather.description, element.valid_date);
-        result.push(weatherData);
+        response.status(200).send(weatherData);
+    }).catch((error) => {
+        console.log('ereor', error);
+        response.staus(500).send('sorry, something wrong');
     });
-    return response.send(result);
 });
 
 app.use('*', (request, response) => {
