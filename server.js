997c6b585d71ffa2231a64edcfae6d050b25a8b3
naeemmusamh <naeem@DESKTOP-RJ6QHBJ.localdomain>
2021-04-01 21:50:37 +0300

    solve some bug

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use(cors());
 
 //A constructor function will ensure that each object is created according to the
 //same format when your server receives the external data.
-function Locations(data, search_query) {
-    this.search_query = search_query;
+function Locations(data, searchQuery) {
+    this.search_query = searchQuery;
     this.formatted_query = data.display_name;
     this.latitude = data.lat;
     this.longitude = data.lon;
@@ -34,17 +34,17 @@ function Locations(data, search_query) {
 //A constructor function will ensure that each object is created according to the
 //same format when the server receives data.
 function Weathers(data) {
-    this.city_name = data.forecast;
-    this.valid_date = data.time;
+    this.forecast = data.weather.description;
+    this.time = data.valid_date;
 }
 
 //A constructor function will ensure that each object is created according to the
 //same format when your server receives the external data.
-function Park(data) {
+function Parks(data) {
     this.name = data.name;
     this.description = data.description;
     this.address = `${data.addresses[0].line1} ${data.addresses[0].city} ${data.addresses[0].statecode} ${data.addresses[0].postalcode}`;
-    this.fees = data.fess[0] || '0.00';
+    this.fee = data.fees[0] || '1.00';
     this.longitude = data.url;
 }
 
@@ -66,11 +66,11 @@ app.get('/location', function(request, response) {
     superagent.get(url).query(cityQuery).then(responseData => {
         // console.log(responseData.body);
         const locationData = new Locations(responseData.body[0], searchQuery);
-        // console.log(locationData);
+        console.log(locationData);
         response.status(200).send(locationData);
     }).catch((error) => {
         console.log('error', error);
-        response.staus(500).send('sorry, something wrong');
+        response.status(500).send('sorry, something wrong');
     });
 });
 
@@ -78,45 +78,39 @@ app.get('/location', function(request, response) {
 //weather object of the result, return an array of objects for each day of the
 //response which contains the necessary information for correct client rendering.
 app.get('/weather', function(request, response) {
-    // let city = request.query.city;
-    const url = `https://api.weatherbit.io/v2.0/history/daily`;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily`;
     const cityQuery = {
         lat: request.query.latitude,
         lon: request.query.longitude,
-        key: WEATHER_API_KEY,
-    };
-    // const weatherRow = require('./data/weather.json');
+        key: WEATHER_API_KEY
+    }
     superagent.get(url).query(cityQuery).then(requestData => {
         const weatherData = requestData.body.data.map(weather => {
-            new Weathers(weather);
+            console.log(weatherData);
+            return new Weathers(weather);
+            // console.log(weatherData);
         });
         response.send(weatherData);
     }).catch((error) => {
         console.log('error', error);
-        response.staus(500).send('sorry, something wrong');
+        response.status(500).send('sorry, something wrong');
     });
 });
 
 //Create a route with a method and a path.
 //weather object of the result, return an array of objects for each day of the
-//response which contains the necessary information for correct client rendering.
+// response which contains the necessary information for correct client rendering.
 app.get('/park', function(request, response) {
-    // let city = request.query.city;
-    const url = `https://developer.nps.gov/api/v1/parks?${request.query.latitude,request.query.longitude}&api_key=${WEATHER_API_KEY}`;
-    const cityQuery = {
-        lat: request.query.latitude,
-        lon: request.query.longitude,
-        key: PARK_API_KEY,
-    };
-    // const weatherRow = require('./data/weather.json');
-    superagent.get(url).query(cityQuery).then(requestData => {
+    const url = `https://developer.nps.gov/api/v1/parks?q=${request.query.search_query}&api_key=${PARK_API_KEY}&limit=10`;
+    superagent.get(url).then(requestData => {
         const parkData = requestData.body.data.map(park => {
-            new Park(park);
+            console.log(parkData);
+            return new Parks(park);
         });
         response.send(parkData);
     }).catch((error) => {
         console.log('error', error);
-        response.staus(500).send('sorry, something wrong');
+        response.status(500).send('sorry, something wrong');
     });
 });
 
